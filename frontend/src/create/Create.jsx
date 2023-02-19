26c477eb2e7228253e061432c43795f129ca05b4
shaxruz25 <shaxruzismoilov250@gmail.com>
2023-02-19 21:55:22 +0500

    todo-list all page full

--- frontend/src/create/Create.jsx
+++ frontend/src/create/Create.jsx
@@ -1,9 +1,22 @@
 import "./Create.css";
 import { Link } from "react-router-dom";
 const Create = () => {
+  const register = (e) => {
+    e.preventDefault();
+    let { text } = e.target;
+    let new_user = {
+      text: text.value,
+    };
+    fetch("http://localhost:3000/create_data", {
+      method: "POST",
+      body: JSON.stringify(new_user),
+    })
+      .then((res) => res.json())
+      .then((data) => window.location.reload(false));
+  };
   return (
     <div className="create">
-      <form className="form">
+      <form onSubmit={(e) => register(e)} className="form">
         <input
           className="form_input"
           type="text"
@@ -15,13 +28,19 @@ const Create = () => {
       <nav>
         <ul className="navbar_item">
           <li className="navbar_list">
-            <Link className="navbar_link">All</Link>
+            <Link to="/" className="navbar_link">
+              All
+            </Link>
           </li>
           <li className="navbar_list">
-            <Link className="navbar_link">Active</Link>
+            <Link to="/active" className="navbar_link">
+              Active
+            </Link>
           </li>
           <li className="navbar_list">
-            <Link className="navbar_link">Completed</Link>
+            <Link to="/completed" className="navbar_link">
+              Completed
+            </Link>
           </li>
         </ul>
       </nav>
